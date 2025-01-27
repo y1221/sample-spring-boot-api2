5a2e842a5f66f756d129db3549903f151ad50568
okayu <yuto.gio.0515@gmail.com>
2025-01-28 08:40:00 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/sample_spring_boot_api2/SampleSpringBootApi2Application.java
@@ -0,0 +1,13 @@
+package com.example.sample_spring_boot_api2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sample_spring_boot_api2/SampleSpringBootApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample_spring_boot_api2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
