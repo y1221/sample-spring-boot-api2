521663326d2f3adab1a8e2ba88271f8d1f8e9e75
okayu <yuto.gio.0515@gmail.com>
2025-01-28 23:57:47 +0900

    [1件取得処理実装] Controllerクラス修正

--- src/main/java/com/example/sample_spring_boot_api2/controller/UserController.java
+++ src/main/java/com/example/sample_spring_boot_api2/controller/UserController.java
@@ -8,7 +8,9 @@ import com.example.sample_spring_boot_api2.service.UserService;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/users")
@@ -23,4 +25,11 @@ public class UserController {
     public List<UserDTO> getAllUsers() {
         return userService.getAllUsers();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        UserDTO userDTO = userService.getUserById(id);
+        return ResponseEntity.ok(userDTO);
+    }
+
 }
