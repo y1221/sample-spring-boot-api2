61f79d7b24c37ed5728bd32bba89bc14b8015930
okayu <yuto.gio.0515@gmail.com>
2025-01-28 09:08:23 +0900

    DTOクラス作成

---
+++ src/main/java/com/example/sample_spring_boot_api2/dto/UserDTO.java
@@ -0,0 +1,10 @@
+package com.example.sample_spring_boot_api2.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private Long id;
+    private String name;
+    private Integer age;
+}
